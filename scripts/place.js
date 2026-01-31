2607f9935403c951a65e6b6e6bb7b28d0b326f40
Andreza Santana <inss.andreza@gmail.com>
2026-01-31 20:29:42 -0300

    ajust

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,4 @@
 
-
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
 
@@ -9,7 +8,7 @@ const windSpeed = 5;
 const calculateWindChill = (t, s) => 13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16);
 
 const displayWindChill = () => {
-    const windChillElement = document.querySelector("#wind-chill-value");
+    const windChillElement = document.querySelector("#windchill");
     
     if (temperature <= 10 && windSpeed > 4.8) {
         const result = calculateWindChill(temperature, windSpeed);
